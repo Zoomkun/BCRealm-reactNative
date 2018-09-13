2cd8474cba42ca7d0df5b340af42e2c95d403c83
Zoomkun <18779186074@163.com>
2018-09-13 11:05:55 +0800

    图标处理

--- src/page/Community/communityInfo/index.js
+++ src/page/Community/communityInfo/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import {
     Container,
@@ -30,25 +30,26 @@ class CommunityInfo extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            chatGroupInfo:{},
-            userInfo:{}
+            chatGroupInfo: {},
+            userInfo: {}
         }
 
     }
+
     static navigationOptions = {
         header: null
     };
 
-    componentWillMount(){
+    componentWillMount() {
         let self = this
         self.setState({
-            chatGroupInfo:self.props.navigation.getParam('item')
+            chatGroupInfo: self.props.navigation.getParam('item')
         })
 
         AsyncStorage.getItem('data').then(data => {
             let userInfo = JSON.parse(data);
             self.setState({
-                userInfo:userInfo
+                userInfo: userInfo
             })
         })
     }
@@ -57,28 +58,28 @@ class CommunityInfo extends Component {
         this.props.navigation.goBack();
     }
 
-    _toChat(){
+    _toChat() {
         let self = this
         const {navigation} = this.props;
         const data = self.state
         const groupInfo = data.chatGroupInfo
         let session = {
             ...groupInfo,
-            sessionType:'0',
-            imToken:data.userInfo.imToken,
-            account:data. userInfo.accountNo,
+            sessionType: '0',
+            imToken: data.userInfo.imToken,
+            account: data.userInfo.accountNo,
         };
         navigation.popToTop()
 
-        NimSession.login(session.account,session.imToken).then((data)=>{
-            navigation.navigate('Chat',{session:session })
-        },(err)=>{
+        NimSession.login(session.account, session.imToken).then((data) => {
+            navigation.navigate('Chat', {session: session})
+        }, (err) => {
             console.warn(err);
         })
     }
 
     // 退群
-    _leaveChat(){
+    _leaveChat() {
         let self = this
         let groupInfo = self.state.chatGroupInfo
         let userInfo = self.state.userInfo
@@ -104,28 +105,44 @@ class CommunityInfo extends Component {
             <Container style={styles.container}
                        data={data}>
                 <Header style={CommonStyles.headerStyle}>
-                    <Button transparent onPress={() => { this.goBack() }}>
-                        <Icon name={"ios-arrow-back"} style={CommonStyles.backIconStyle} />
+                    <Button transparent onPress={() => {
+                        this.goBack()
+                    }}>
+                        <Icon name={"ios-arrow-back"} style={CommonStyles.backIconStyle}/>
                     </Button>
                     <Body style={CommonStyles.titleBodyStyle}>
-                        <Text style={CommonStyles.headertextStyle}>社区详情</Text>
+                    <Text style={CommonStyles.headertextStyle}>社区详情</Text>
                     </Body>
                 </Header>
                 <View style={styles.top}>
-                    <Thumbnail large style={styles.image} source={{ uri: data.icon }} />
-                    <Text style={{ color: '#333',paddingBottom:20 }}>{data.chatGroupName}</Text>
+                    <Thumbnail large style={styles.image} source={{uri: data.icon}}/>
+                    <Text style={{color: '#333', paddingBottom: 20}}>{data.chatGroupName}</Text>
                 </View>
                 <Content style={styles.content}>
-                    <Text style={styles.title}>群二维码</Text>
-                    <Text style={styles.title}>群成员</Text>
+                    <View>
+                        <Text style={styles.title}>群二维码</Text>
+                        <Right>
+                            <Image
+                                source={require('../../../images/qrCode.png')}
+                                style={CommonStyles.icon}
+                            />
+                            <Image
+                                source={require('../../../images/goIn.png')}
+                                style={CommonStyles.icon}
+                            />
+                        </Right>
+                    </View>
+                    <View>
+                        <Text style={styles.title}>群成员</Text>
+                    </View>
                     <Text style={styles.title}>群公告</Text>
-                    <Text style={{ fontSize: 14, lineHeight: 18 }}>
+                    <Text style={{fontSize: 14, lineHeight: 18}}>
                         {data.announcement}
                     </Text>
-                    <Button full style={{ backgroundColor: '#FE6F06', marginTop: 20 }}  onPress={()=>this._toChat()}>
+                    <Button full style={{backgroundColor: '#FE6F06', marginTop: 20}} onPress={() => this._toChat()}>
                         <Text style={styles.colorWhite}>进入该群</Text>
                     </Button>
-                    <Button full style={{ backgroundColor: '#FE0606', marginTop: 20 }}  onPress={()=>this._leaveChat()}>
+                    <Button full style={{backgroundColor: '#FE0606', marginTop: 20}} onPress={() => this._leaveChat()}>
                         <Text style={styles.colorWhite}>退出该群</Text>
                     </Button>
                 </Content>
